f08435069b81b0fa674c52c5641178b1a37cdab6
Dorian Trehet <dorian.trehet@gmail.com>
2025-01-12 00:31:55 +0100

    Change design Animal Card

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -2,26 +2,30 @@ import React from 'react';
 import './AnimalCard.css';
 import TinderCard from 'react-tinder-card';
 
-const AnimalCard = ({ name, level, price, stats, imageUrl, onSwipe }) => {
-  return (
-    <TinderCard onSwipe={(dir) => onSwipe(dir)}>
-      <div className="animal-card">
-        <img src={imageUrl} alt={name} className="animal-image" />
-        <h3>{name}</h3>
-        <p>Niveau : {level}</p>
-        <p>Prix : {price.toLocaleString('fr-FR')} Kamas</p>
-        <div className="stats">
-          <h4>Caractéristiques :</h4>
-          <ul>
-            {stats.map((stat, index) => (
-              <li key={index}>
-                {stat.name} : {stat.value}
-              </li>
-            ))}
-          </ul>
-        </div>
+const AnimalCard = ({ name, level, price, stats, imageUrl, onSwipe, isSwipeable = true }) => {
+  const cardContent = (
+    <div className="animal-card">
+      <img src={imageUrl} alt={name} className="animal-image" />
+      <h3>{name}</h3>
+      <p>Niveau : {level}</p>
+      <div className="stats">
+        <h4>Caractéristiques :</h4>
+          {stats.map((stat, index) => (
+            <p key={index}>
+              {stat.name} : {stat.value}
+            </p>
+          ))}
+      <h4>Prix : {price.toLocaleString('fr-FR')} Kamas</h4>
       </div>
+    </div>
+  );
+
+  return isSwipeable ? (
+    <TinderCard onSwipe={(dir) => onSwipe(dir)}>
+      {cardContent}
     </TinderCard>
+  ) : (
+    <div>{cardContent}</div>
   );
 };
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './Profile.css';
+import AnimalCard from './AnimalCard';
 
 const Profile = () => {
   const [user, setUser] = useState({
@@ -15,80 +17,64 @@ const Profile = () => {
     const fetchUserData = async () => {
       try {
         const token = localStorage.getItem('token');
-
         if (!token) {
-          console.log('Token is missing. Redirecting to login.');
           window.location.href = '/login';
           return;
         }
-
         const response = await axios.get('http://localhost:5000/current-user', {
           headers: {
             'x-auth-token': token,
           },
         });
-
         setUser(response.data);
       } catch (error) {
-        console.error('Error fetching user data:', error);
         if (error.response && error.response.status === 401) {
-          console.log('Token is invalid or expired. Redirecting to login.');
           window.location.href = '/login';
         }
       }
     };
-
     fetchUserData();
   }, []);
+
+  const handleSwipe = (direction) => {
+    console.log(`Animal swiped in the ${direction} direction`);
+  };
 
   return (
     <div>
       <h2>Profile</h2>
       <div>
-        <div>
-          <strong>Name: </strong>
-          <span>{user.name}</span>
-        </div>
-
-        <div>
-          <strong>Email: </strong>
-          <span>{user.email}</span>
-        </div>
-
-        <div>
-          <strong>In-game Username: </strong>
-          <span>{user.inGameUsername}</span>
-        </div>
-
-        <div>
-          <strong>Server: </strong>
-          <span>{user.server}</span>
-        </div>
+        <strong>Name:</strong> <span>{user.name}</span>
       </div>
-
+      <div>
+        <strong>Email:</strong> <span>{user.email}</span>
+      </div>
+      <div>
+        <strong>In-game Username:</strong> <span>{user.inGameUsername}</span>
+      </div>
+      <div>
+        <strong>Server:</strong> <span>{user.server}</span>
+      </div>
+      
       <div>
         <h3>Animals for Adoption</h3>
         {user.animalsForAdoption.length > 0 ? (
-          <ul>
-            {user.animalsForAdoption.map((animal, index) => (
-              <li key={index}>{animal.name} - {animal.race}</li>
+          <div className="animal-cards-container">
+            {user.animalsForAdoption.map((animal) => (
+              <AnimalCard
+                key={animal._id}
+                name={animal.name}
+                level={animal.level}
+                price={animal.price}
+                stats={animal.stats}
+                imageUrl={animal.imageUrl}
+                onSwipe={handleSwipe}
+                isSwipeable={false}
+              />
             ))}
-          </ul>
+          </div>
         ) : (
           <p>No animals for adoption</p>
-        )}
-      </div>
-
-      <div>
-        <h3>Adopted Animals</h3>
-        {user.adoptedAnimals.length > 0 ? (
-          <ul>
-            {user.adoptedAnimals.map((animal, index) => (
-              <li key={index}>{animal.name} - {animal.race}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No animals adopted</p>
         )}
       </div>
     </div>
