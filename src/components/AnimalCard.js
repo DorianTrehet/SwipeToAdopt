3a43e37052c37928b18ce86e8fc17b667f6e2e0b
Dorian Trehet <dorian.trehet@gmail.com>
2025-01-12 14:26:13 +0100

    coeur animation

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AnimalCard.css';
 import TinderCard from 'react-tinder-card';
 
 const AnimalCard = ({ name, level, price, stats, imageUrl, onSwipe, isSwipeable = true }) => {
+  const [showHeart, setShowHeart] = useState(false);
+
+  const handleSwipe = (dir) => {
+    if (dir === 'right') {
+      setShowHeart(true);
+      setTimeout(() => setShowHeart(false), 500);  // Cache le cœur après l'animation
+    }
+    onSwipe(dir);
+  };
+
   const cardContent = (
     <div className="animal-card">
+      {showHeart && <div className="heart-animation">❤️</div>}
       <img src={imageUrl} alt={name} className="animal-image" />
       <h3>{name}</h3>
       <p>Niveau : {level}</p>
       <div className="stats">
         <h4>Caractéristiques :</h4>
-          {stats.map((stat, index) => (
-            <p key={index}>
-              {stat.name} : {stat.value}
-            </p>
-          ))}
-      <h4>Prix : {price.toLocaleString('fr-FR')} Kamas</h4>
+        {stats.map((stat, index) => (
+          <p key={index}>
+            {stat.name} : {stat.value}
+          </p>
+        ))}
+        <h4>Prix : {price.toLocaleString('fr-FR')} Kamas</h4>
       </div>
     </div>
   );
 
   return isSwipeable ? (
-    <TinderCard onSwipe={(dir) => onSwipe(dir)}>
+    <TinderCard onSwipe={handleSwipe}>
       {cardContent}
     </TinderCard>
   ) : (
